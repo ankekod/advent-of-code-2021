3077d8d7a74aa14dac8e9111e7c672175d5792e7
Jonathan Ström <jonathanstroem91@gmail.com>
2021-12-06 20:26:05 +0100

    Added script to count number of depths

---
+++ 1st/main.py
@@ -0,0 +1,22 @@
+
+file = open("input.txt", "r")
+depths = file.read().split()
+
+def check_depth(depths):
+    previous_depth = depths[0]
+    print(previous_depth + " (N/A - no previous measurement)")
+
+    increases, decreases = 0, 0
+    for depth in depths[1:]:
+        if int(depth) > int(previous_depth):
+            increases += 1
+            print(depth + " (increased)")
+        else:
+            decreases += 1
+            print(depth + " (decreased)")
+        previous_depth = depth
+
+    return increases, decreases
+
+increases, decreases = check_depth(depths)
+print("There were {increases} increases and {decreases} decreases in depth respectively.".format(increases = increases, decreases = decreases))
